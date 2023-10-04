7d2f2eb5424e2f29553b94a542aa9c846558cbc2
vegayes <vegayes@naver.com>
2023-10-04 17:03:35 +0900

    eunseo

--- my-page.js
+++ my-page.js
@@ -1,6 +1,6 @@
 // 1) 즐겨찾기 수정 버튼 누르면
 document.getElementById("favorite-popup").addEventListener("click",function(){
-    window.open("http://192.168.219.108:5501/mypage-create-main/my-page-popup-favorite.html",
+    window.open("http://192.168.50.200:5501/my-page-popup-favorite.html",
     "팝업 즐겨찾기",
     "width=1200, height=850, top=100, left=400");
 });
@@ -44,34 +44,61 @@ document.getElementsByName("comment-check").forEach(function(v) {
 });
 
 
+// 3) 즐겨찾기 -> 영화관, 영화 구분하기
+const movieBtn = document.getElementById("movie-favorite-btn");
+const cinemaBtn = document.getElementById("cinema-favorite-btn");
+
+const favoriteMovieContainer =document.getElementById("movie-favorite-container");
+const favoriteCinemaContainer =document.getElementById("cinema-favorite-container");
 
 
 
-// 3) 스크롤하기
-var swiper = new Swiper('.favorite-list', {
-  // direction: 'horizontal',
-  autoplay: true,
-  loop: true,
-  spaceBetween: 30,
-  slidesPerView: 3,
-  navigation: {
-    // prevEl: '.awards .swiper-prev',
-    nextEl: '.next-btn-container'
-  }
+cinemaBtn.addEventListener("click" ,function(){
+    
+  console.log("영화 누름");
+
+  cinemaBtn.style.opacity = "1";
+  cinemaBtn.style.color = "black";
+
+  movieBtn.style.opacity = "0.5";
+
+  favoriteMovieContainer.style.display = "none";
+  favoriteCinemaContainer.style.display = "";
+});
+
+
+movieBtn.addEventListener("click" ,function(){
+    
+  console.log("영화관 누름");
+
+  cinemaBtn.style.opacity = "0.5";
+
+
+  movieBtn.style.opacity = "1";
+  movieBtn.style.color = "black";
+
+  favoriteMovieContainer.style.display = "";
+  favoriteCinemaContainer.style.display = "none";
 });
 
 
 
 
+// 4) 스크롤하기
+// var swiper = new Swiper('.favorite-list', {
+//   // direction: 'horizontal',
+//   autoplay: true,
+//   loop: true,
+//   spaceBetween: 30,
+//   slidesPerView: 3,
+//   navigation: {
+//     // prevEl: '.awards .swiper-prev',
+//     nextEl: '.next-btn-container'
+//   }
+// });
 
 
 
-
-
-
-
-
-// 4) 즐겨찾기 -> 영화관, 영화 구분하기
 
 // 5) 
 
