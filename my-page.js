813badf74c26aa621682ebdd76c638d80b691936
vegayes <vegayes@naver.com>
2023-10-22 22:19:25 +0900

    eunseo

--- my-page.js
+++ my-page.js
@@ -36,6 +36,54 @@ const commnetOpen = document.getElementById("comment-list-edit");
 
 
 
+// 1) 팝업창 띄우기
+const modal = document.getElementById("favorite-list-modal-box");
+const modalContent = document.getElementById("favorite-list-modal-content");
+const openModalBtn = document.getElementById("favorite-popup-open");
+const closeModalBtn = document.getElementById("fv-modal-close");
+const closeModalBack = document.getElementById("fv-modal-back");
+
+function modalOpen(){
+    modal.style.display ="flex";
+}
+
+function modalClose(){
+
+  console.log("닫기");
+    modal.style.display = "none";
+}
+
+
+// 모달창 열기
+openModalBtn.addEventListener("click", modalOpen);
+// 모달창 닫기
+closeModalBtn.addEventListener("click", modalClose);
+closeModalBack.addEventListener("click", modalClose);
+
+
+const modalCM = document.getElementById("comment-update-modal-box");
+const modalCMContent = document.getElementById("comment-update-content");
+const openModalCMBtn = document.getElementById("cmPopup");
+const closeModalCMBtn = document.getElementById("cm-modal-close");
+const closeModalCMBack = document.getElementById("cm-modal-back");
+
+function modalCMOpen(){
+    modalCM.style.display ="flex";
+}
+
+function modalCMClose(){
+
+  console.log("닫기");
+    modalCM.style.display = "none";
+}
+
+
+// 모달창 열기
+openModalCMBtn.addEventListener("click", modalCMOpen);
+// 모달창 닫기
+closeModalCMBtn.addEventListener("click", modalCMClose);
+closeModalCMBack.addEventListener("click", modalCMClose);
+
 
 
 
